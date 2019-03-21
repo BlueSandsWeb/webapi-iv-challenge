048945144df6760fce8fe07d3c5b483320e9421d
BlueSandsWeb <jon@bluesandswebdesign.com>
2019-03-21 15:10:38 -0700

    changed index file to make port dynamic.

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-const server = require('./server.js');
+const server = require("./server.js");
+require("dotenv").config();
 
-server.listen(4000, () => {
-        console.log('\n Server Running on http://localhost:4000');
-})
+const port = process.env.PORT || 5000;
+
+server.listen(port, () => {
+  console.log(`\n Server Running on http://localhost:${port}`);
+});
